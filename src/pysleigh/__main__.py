683e317c6572335747de5ba59507d84ea8c316bf
Roee Shoshani <roysho10@gmail.com>
2025-11-20 20:39:03 +0200

    add main code

--- src/pysleigh/__main__.py
+++ src/pysleigh/__main__.py
@@ -1,7 +1,11 @@
 from .pysleigh_bindings import Sleigh, SimpleLoadImage
+from pathlib import Path
 
 class MyLoadImage(SimpleLoadImage):
     def loadSimple(addr, amount):
         return b'\x00'*amount
 
-s = Sleigh(b'hello', MyLoadImage())
+PROCESSORS_DIR = Path(__file__).parent.joinpath('processors')
+sla_path = PROCESSORS_DIR.joinpath('x86/data/languages/x86-64.sla')
+sla_path_str = str(sla_path)
+s = Sleigh(sla_path_str, MyLoadImage())
